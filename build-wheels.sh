f148da2957ae8d4f11830f683f6fc9dee56b9316
Rene Milk <rene.milk@wwu.de>
2016-10-07 10:15:58 +0200

    do not build wheels for 2.6+3.3

--- build-wheels.sh
+++ build-wheels.sh
@@ -4,19 +4,22 @@ set -e -x
 # Install a system package required by our library
 yum install -y atlas-devel
 
+# we do not build wheels for:
+# cPython 2.6 - pymor's incompatible with it
+# cPython 3.3 - there's no scipy wheel for it
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYBIN in /opt/python/cp3{4,5}*/bin /opt/python/cp27*/bin; do
     ${PYBIN}/pip install -r /io/pymor/requirements-rtd.txt
     ${PYBIN}/pip wheel /io/pymor/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in wheelhouse/pymor*.whl; do
     auditwheel repair $whl -w /io/wheelhouse/
 done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
+for PYBIN in /opt/python/cp3{4,5}*/bin /opt/python/cp27*/bin; do
     ${PYBIN}/pip install pymor --no-index -f /io/wheelhouse
     (cd $HOME; ${PYBIN}/py.test pymor)
 done
