0515cd64b6f1cd8510928a955a13405284615eb0
Rene Milk <rene.milk@wwu.de>
2016-10-07 16:47:25 +0200

    refactor wheel dir usage

--- build-wheels.sh
+++ build-wheels.sh
@@ -8,18 +8,21 @@ yum install -y atlas-devel
 # cPython 2.6 - pymor's incompatible with it
 # cPython 3.3 - there's no scipy wheel for it
 # Compile wheels
+
+WHEEL_DIR=/io/wheelhouse
+
 for PYBIN in /opt/python/cp3{4,5}*/bin /opt/python/cp27*/bin; do
     ${PYBIN}/pip install -r /io/pymor/requirements-rtd.txt
-    ${PYBIN}/pip wheel /io/pymor/ -w wheelhouse/
+    ${PYBIN}/pip wheel /io/pymor/ -w ${WHEEL_DIR}/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/pymor*.whl; do
-    auditwheel repair $whl -w /io/wheelhouse/
+for whl in ${WHEEL_DIR}/pymor*.whl; do
+    auditwheel repair $whl -w ${WHEEL_DIR}/
 done
 
 # Install packages and test
 for PYBIN in /opt/python/cp3{4,5}*/bin /opt/python/cp27*/bin; do
-    ${PYBIN}/pip install -vvv --pre  pymor --no-index -f /io/wheelhouse
+    ${PYBIN}/pip install -vvv --pre  pymor --no-index -f file://${WHEEL_DIR}
     (cd $HOME; ${PYBIN}/py.test pymor)
 done
