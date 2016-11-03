57013486004bebdc014a8e407bd0072124bd48bd
Rene Milk <rene.milk@wwu.de>
2016-11-03 12:43:33 +0100

    [travis] reqire to test wheel outside the docker container

--- build-wheels.sh
+++ build-wheels.sh
@@ -29,6 +29,3 @@ for whl in ${WHEEL_DIR}/pymor*.whl; do
     auditwheel repair $whl -w ${WHEEL_DIR}/
 done
 
-# Install packages and test
-${PYBIN}/pip install --pre --no-index -f file://${WHEEL_DIR} ${WHEEL_DIR}/pymor-*${PYVER}-manylinux*.whl
-/io/xvfb-run -a ${PYBIN}/py.test --pyargs pymortests -c /io/pymor/.installed_pytest.ini
