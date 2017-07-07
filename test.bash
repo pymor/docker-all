e9da4b25684192af221bbe8ef3d8e5f0621f315d
René Milk <rene.milk@wwu.de>
2017-07-07 22:08:07 +0200

    update xvfb-run and use it in test script

--- test.bash
+++ test.bash
@@ -8,4 +8,4 @@ python -c 'import distutils.util; print(distutils.util.get_platform())'
 pip install -U pip
 pip install --pre --no-index -f file://${PWD}/wheelhouse/ wheelhouse/pymor-*-${PYVER}-manylinux1_x86_64.whl
 pip install -r pymor/requirements-optional.txt
-xvfb-run -a py.test --pyargs pymortests -c ./pymor/.ci/installed_pytest.ini
+./xvfb-run -a py.test --pyargs pymortests -c ./pymor/.ci/installed_pytest.ini
