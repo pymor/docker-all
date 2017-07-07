0314bdc3dabba43d0f68a79f2f3e30414219d38a
Rene Milk <rene.milk@wwu.de>
2017-07-07 16:27:51 +0200

    rewire everything
    - adds a docker image that keeps manylinux base + our extra rpm installs
    - add a nice Makefile that does everythin
    - test installed wheel in a clean virtualenv

---
+++ Makefile
@@ -0,0 +1,28 @@
+.PHONY: all wheel docker 2.7 3.4 3.5
+
+DOCKER_IMAGE=
+BRANCH=py3_debs
+
+3.5: REV=3.5
+3.5: PYVER=cp35-cp35m
+3.5: wheel
+
+3.4: REV=3.4
+3.4: PYVER=cp34-cp34m
+3.4: wheel
+
+2.7: REV=2.7
+2.7: PYVER=cp27-cp27mu
+2.7: wheel
+
+wheel: docker
+	[ -d pymor ] || git clone https://github.com/pymor/pymor
+	cd pymor && git fetch && git reset --hard $(BRANCH) && cd -
+	docker run --rm -v ${PWD}:/io -e PYVER="$(PYVER)" pymor/manylinux:latest /io/build-wheels.sh
+	ls wheelhouse/
+	# Install packages and test
+	REV=$(REV) PYVER=$(PYVER) ./test.bash
+
+docker:
+	docker build -t pymor/manylinux:latest docker/
+
--- build-wheels.sh
+++ build-wheels.sh
@@ -5,23 +5,13 @@ if [ -z ${PYVER+x} ]; then
     PYVER="cp34-cp34m"
 fi
 
-# Install required system packages
-yum install -y atlas-devel openmpi-devel fltk freeglut libpng libjpeg tk tcl xorg-x11-server-Xvfb xauth
-rpm -ivh /io/gmsh-r8692-1.el5.x86_64.rpm
-
-# enable mpi4py compilation
-export MPICC=/usr/lib64/openmpi/1.4-gcc/bin/mpicc
-
 # Compile wheels
 PYBIN=/opt/python/${PYVER}/bin
 WHEEL_DIR=/io/wheelhouse
-REQ=/io/requirements.txt
-if [[ -f /io/requirements_${PYVER}.txt ]] ; then
-    REQ=/io/requirements_${PYVER}.txt
-fi
+
 # evtk fails to install w/o numpy present
-${PYBIN}/pip install numpy
-${PYBIN}/pip install -r ${REQ}
+${PYBIN}/pip install -r /io/pymor/requirements.txt
+
 ${PYBIN}/pip wheel /io/pymor/ -w ${WHEEL_DIR}/
 
 # Bundle external shared libraries into the wheels
