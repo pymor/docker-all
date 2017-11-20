eee42aec8868e931483012a2beacfffa6dcc9d81
René Milk <rene.milk@wwu.de>
2017-11-20 10:24:35 +0100

    [builder] default to NOT build from dirty git state

--- builder_docker/build-wheels.sh
+++ builder_docker/build-wheels.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/bin/bash -l
 set -e -x
+
+# do not build from dirty git unless DIRTY_BUILD evaluates to true
+pushd /io/pymor
+if [[ $(git rev-parse --show-toplevel 2>/dev/null) = "$PWD" ]] ; then
+    [[ ${DIRTY_BUILD} ]] || git diff --exit-code
+fi
+popd
 
 # Compile wheels
 
