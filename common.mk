d19d559a001edc52a6d3bddc641722a59d7781a0
René Fritze <rene.fritze@wwu.de>
2020-02-19 12:29:21 +0100

    add pypi-mirror image

--- common.mk
+++ common.mk
@@ -7,11 +7,12 @@ FENICS_IMAGE = pymor/fenics_py$1:$2
 DEALII_IMAGE = pymor/dealii_py$1:$2
 CIBASE_IMAGE = pymor/cibase_py$1:$2
 TESTING_IMAGE = pymor/testing_py$1:$2
+PYPI_MIRROR_STABLE_IMAGE = pymor/pypi-mirror_stable_py$1:$2
 # DOCKER_BUILD=docker build --squash
 DOCKER_BUILD=docker build --cache-from=$(call TESTING_IMAGE,$@,latest) --cache-from=$(call CIBASE_IMAGE,$@,latest) \
 	--cache-from=$(call DEALII_IMAGE,$@,latest) --cache-from=$(call FENICS_IMAGE,$@,latest) \
 	--cache-from=$(call PYTHON_IMAGE,$@,latest) --cache-from=$(call PETSC_IMAGE,$@,latest) \
-	--cache-from=$(call NGSOLVE_IMAGE,$@,latest)
+	--cache-from=$(call NGSOLVE_IMAGE,$@,latest) --cache-from=$(call PYPI_MIRROR_STABLE_IMAGE,$@,latest)
 DOCKER_TAG=docker tag
 DOCKER_PUSH=docker push
 DOCKER_PULL=docker pull
