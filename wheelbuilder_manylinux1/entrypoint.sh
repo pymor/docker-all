e69141500da587cc4fb9efd8b5f479f4caa80d05
René Fritze <rene.fritze@wwu.de>
2020-07-01 11:28:54 +0200

    [wheelbuilder] remove sudo usage

--- wheelbuilder_manylinux1/entrypoint.sh
+++ wheelbuilder_manylinux1/entrypoint.sh
@@ -5,7 +5,6 @@ GROUP_ID=${LOCAL_GROUP_ID:-1000}
 
 echo "Starting with UID : $USER_ID"
 useradd  --shell /bin/bash -u $USER_ID -o  -m pymor
-echo 'pymor ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 export HOME=/home/pymor
 
 exec gosu pymor "$@"
