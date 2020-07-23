3c804f0639536dd4b319ebcbf3a3c82e547e345e
Rene Milk <rene.milk@uni-muenster.de>
2020-07-23 17:32:10 +0200

    add new demo to makefile

--- common.mk
+++ common.mk
@@ -61,4 +61,4 @@ PETSC_TAG=$(VER)
 PYMOR_BRANCH=master
 MANYLINUXS=1 2010 2014
 DISTROS = centos_8 debian_stretch debian_buster debian_bullseye
-DEMO_TAGS = 0.5 master 2019.2
+DEMO_TAGS = 0.5 master 2019.2 2020.1
