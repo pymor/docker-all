211c95f0623525c308b8b414774683612cd17da4
René Fritze <rene.fritze@wwu.de>
2020-07-24 15:12:05 +0200

    start building for python 3.9

--- .ci/template.azure.py
+++ .ci/template.azure.py
@@ -121,7 +121,7 @@ from itertools import product
 from pathlib import Path
 
 tpl = jinja2.Template(tpl)
-pythons = ['3.6', '3.7', '3.8']
+pythons = ['3.6', '3.7', '3.8', '3.9']
 manylinux = ['1', '2010', '2014']
 
 with open(os.path.join(os.path.dirname(__file__), 'azure-pipeline.yml'), 'wt') as yml:
