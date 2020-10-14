6c4519f0a74dc0cec1cf27a45e712ab3f3862ec7
René Fritze <rene.fritze@wwu.de>
2020-10-14 13:59:49 +0200

    special case pytorch in constraints

--- constraints/constraints.bash
+++ constraints/constraints.bash
@@ -1,15 +1,17 @@
-set -ex
+#!/usr/bin/env bash
+set -exo pipefail
 # fenics and pymor-deallii packages need to be filtered from the freeze command since they're already installed
 # in the constrainer base image and cannot be installed from pypi
 
 REQUIREMENTS="requirements.txt requirements-ci.txt requirements-optional.txt requirements-docker-other.txt"
+pip install /usr/local/share/torch*whl
 
 cd /requirements/
 for fn in ${REQUIREMENTS} ; do
     pip install -r ${fn}
 done
 
-pip freeze --all | grep -v pymess | grep -v fenics | grep -v pymor-dealii | grep -v torch \
+pip freeze --all | grep -v pymess | grep -v fenics | grep -v torch | grep -v dolfin |grep -v dealii \
   > /requirements/constraints.txt
 
 cd /requirements/
@@ -19,9 +21,10 @@ done
 
 cd /requirements/
 virtualenv /tmp/venv_old
+/tmp/venv_old/bin/pip install /usr/local/share/torch*whl
 for fn in oldest_require*.txt ; do
     /tmp/venv_old/bin/pip install -r ${fn}
 done
 
-/tmp/venv_old/bin/pip freeze --all | grep -v pymess | grep -v fenics | grep -v pymor-dealii | grep -v torch \
+/tmp/venv_old/bin/pip freeze --all | grep -v pymess | grep -v fenics | grep -v torch | grep -v dolfin |grep -v dealii \
   > /requirements/oldest_constraints.txt
