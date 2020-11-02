32be675f36a92c0f5be2ff8422ad3099458666fc
René Fritze <rene.fritze@wwu.de>
2020-11-02 09:20:18 +0100

    properly fix mirror with incomplete metadata wheels

--- constraints/constraints.bash
+++ constraints/constraints.bash
@@ -2,16 +2,18 @@ set -ex
 # fenics and pymor-deallii packages need to be filtered from the freeze command since they're already installed
 # in the constrainer base image and cannot be installed from pypi
 
+REQUIREMENTS="requirements.txt requirements-ci.txt requirements-optional.txt requirements-docker-other.txt"
+
 cd /requirements/
-for fn in requirements*.txt ; do
+for fn in ${REQUIREMENTS} ; do
     pip install -r ${fn}
 done
 
-pip freeze --all | grep -v pymess | grep -v fenics | grep -v pymor-dealii \
+pip freeze --all | grep -v pymess | grep -v fenics | grep -v deal \
   > /requirements/constraints.txt
 
 cd /requirements/
-for fn in *.txt ; do
+for fn in ${REQUIREMENTS} ; do
     pypi_minimal_requirements_pinned ${fn} oldest_${fn}
 done
 
