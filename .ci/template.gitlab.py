3f3b928e21e2c97417b4a05e6301f85ba15f1c96
René Fritze <rene.fritze@wwu.de>
2021-01-25 16:16:13 +0100

    follow master-> main rename in pymor repo

--- .ci/template.gitlab.py
+++ .ci/template.gitlab.py
@@ -76,7 +76,7 @@ from pathlib import Path
 tpl = jinja2.Template(tpl)
 pythons = ['3.6', '3.7', '3.8', '3.9']
 manylinux = ['2010', '2014']
-static_targets = ['docker-in-docker', 'docs', 'demo_master', 'deploy_checks', 'ci_sanity']
+static_targets = ['docker-in-docker', 'docs', 'demo_main', 'deploy_checks', 'ci_sanity']
 parameterized_targetss = ['python_builder', 'python', 'constraints', 'dealii', 'petsc', 'pypi-mirror_stable', 'pypi-mirror_oldest',
 'ngsolve', 'fenics' ] + [f'wheelbuilder_manylinux{ml}' for ml in manylinux] +  ['cibase', 'testing','jupyter']
 
