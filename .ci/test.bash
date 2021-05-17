eb859aaa304016386fa0a0881cc5629e46743674
René Fritze <rene.fritze@wwu.de>
2021-05-17 16:25:59 +0200

    [ci] fix import tests

--- .ci/test.bash
+++ .ci/test.bash
@@ -6,6 +6,6 @@ git clone https://github.com/pymor/pymor /tmp/src
 cd /tmp/src
 pip install .[full,docs,ci]
 python -c "from pymor.basic import *"
-python -c "from Qt.QtWidgets import *"
+python -c "from qtpy.QtWidgets import *"
 python -c "from dolfin import *"
 python -c "from ngsolve import *"
