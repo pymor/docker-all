d9785d57e589649535f4a744d643bfe2d4e814bf
René Fritze <rene.fritze@wwu.de>
2021-05-18 14:22:02 +0200

    check requirements after install

--- constraints/constraints.bash
+++ constraints/constraints.bash
@@ -10,7 +10,12 @@ pip install /ci_wheels/*whl
 
 cd /requirements/
 for fn in ${REQUIREMENTS} ; do
-    pip install -r ${fn}
+    PARG="-r ${fn} ${PARG}"
+done
+pip install ${PARG}
+
+for fn in ${REQUIREMENTS} ; do
+    check_reqs.py ${fn}
 done
 
 pip freeze --all | grep -v fenics | grep -v dolfin |grep -v dealii \
@@ -26,8 +31,14 @@ virtualenv /tmp/venv_old
 
 /tmp/venv_old/bin/pip install /ci_wheels/*whl
 for fn in oldest_require*.txt ; do
-    /tmp/venv_old/bin/pip install --use-deprecated=legacy-resolver -r ${fn}
+    VARG="-r ${fn} ${VARG}"
 done
+/tmp/venv_old/bin/pip install ${VARG}
+# currently cannot work due to file-by-file processing of pypi_minimal_requirements_pinned
+# for fn in oldest_require*.txt ; do
+#     check_reqs.py ${fn}
+# done
+
 
 # torch is still excluded here since it cannot be installed from pypi
 /tmp/venv_old/bin/pip freeze --all | grep -v fenics | grep -v torch | grep -v dolfin |grep -v dealii \
